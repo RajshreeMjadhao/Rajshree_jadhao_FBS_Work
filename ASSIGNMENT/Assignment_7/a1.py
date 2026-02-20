257b4f387f7d072cbd69dd26f37902037724eff7
Rajshree Jadhao <166318551+RajshreeMjadhao@users.noreply.github.com>
2026-02-20 22:27:31 +0530

    Add files via upload

---
+++ ASSIGNMENT/Assignment_7/a1.py
@@ -0,0 +1,16 @@
+n = 4
+
+for i in range(n):
+    print(" " * (n - i - 1), end="")
+    print("*", end="")
+    if i > 0:
+        print(" " * (2*i - 1) + "*", end="")
+    print()
+
+
+for i in range(n -2, -1, -1):
+    print(" " * (n - i - 1), end="")
+    print("*", end="")
+    if i > 0:
+        print(" " * (2*i - 1) + "*", end="")
+    print()
---
+++ ASSIGNMENT/Assignment_7/c1.py
@@ -0,0 +1,17 @@
+n = 5
+
+for i in range(1, n+1):
+    print("  " * (n - i), end="")
+
+
+    num = i
+    for j in range(i):
+        print(num, end=" ")
+        num += 1
+
+    num -= 2
+    for j in range(i-1):
+        print(num, end=" ")
+        num -= 1
+
+    print()
